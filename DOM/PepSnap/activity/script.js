326bd5d49b6abeef73cdafc688d71e8a0d1f9d4f
ritik <khokharritik1999@gmail.com>
2021-10-20 12:59:43 +0530

    media recorder

--- DOM/PepSnap/activity/script.js
+++ DOM/PepSnap/activity/script.js
@@ -1,14 +1,58 @@
 
 let videoPlayer = document.querySelector("video");
 let constraints = {video:true};
+let recordButton = document.querySelector("#record-vedio");
+let photoButton = document.querySelector("#capture-photos");
+let recordedData;
+let recordingState = false;
+
 
 (async function(){
     // let devices = await navigator.mediaDevices.enumerateDevices();
     // console.log(devices);
+try
 
-
-    let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+   { let mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
     console.log(mediaStream);
 
-    videoPlayer.srcObject = mediaStream;    
+    videoPlayer.srcObject = mediaStream;
+    mediaRecorder = new MediaRecorder(mediaStream);
+    console.log(mediaRecorder);
+    mediaRecorder.onstart = function(e)
+    {
+        console.log("recording start");
+        console.log(e);
+    }
+
+    mediaRecorder.onstop = function(e)
+    {
+        console.log("recording stop");
+        console.log(e);
+    }
+    mediaRecorder.ondataavailable = function(e)
+    {
+        console.log("recording saved");
+        console.log(e);
+    }
+   
+   recordButton.addEventListener("click",function(){
+     if(recordingState)
+     {
+      mediaRecorder.stop();
+      recordingState = false;
+      recordButton.innerHTML = "Record";
+     }
+     else
+     {
+      mediaRecorder.start();
+      recordingState = true;
+      recordButton.innerHTML = "Recording";
+     }
+   })
+}
+
+   catch(error)
+   {
+
+   }    
 })();
